9138c68205a2dbaa7ccc36cf98eeb5e8909e5bdf
Amber Wang <wangshengyan6@gmail.com>
2024-07-03 18:04:59 +0800

    build:header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,13 @@
 import React from "react";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
+    <nav className="navbar sticky-top navbar-expand-lg bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <a className={`navbar-brand ${styles.navbarBrand}`} href="#">
           My Booklist
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
